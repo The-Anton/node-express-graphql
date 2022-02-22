a67381e3e22db0f87a840828609d8bbb31944fa0
The-Anton <shekhar59324@gmail.com>
2022-02-22 21:56:46 +0530

    add: return single object

--- server.js
+++ server.js
@@ -78,11 +78,32 @@ const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: () => ({
+        book: {
+            type: BookType,
+            deprecation: 'Single Book',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => books.find(book => book.id === args.id) 
+        },
         books: {
             type: new GraphQLList(BookType),
             description: 'List of all books',
             resolve: () => books
-        }
+        },
+        author: {
+            type: AuthorType,
+            deprecation: 'Single Author',
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => authors.find(author => author.id === args.id) 
+        },
+        authors: {
+            type: new GraphQLLint(AuthorType),
+            description: 'List of all authors',
+            resolve: () => authors
+        },
     })
 })
 
