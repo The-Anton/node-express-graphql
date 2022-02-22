607d1410cf881f482ef3b495467f7fd5ceee2aae
The-Anton <shekhar59324@gmail.com>
2022-02-22 22:17:10 +0530

    add: add books and author operation/mutation

--- server.js
+++ server.js
@@ -100,15 +100,47 @@ const RootQueryType = new GraphQLObjectType({
             resolve: (parent, args) => authors.find(author => author.id === args.id) 
         },
         authors: {
-            type: new GraphQLLint(AuthorType),
+            type: new GraphQLList(AuthorType),
             description: 'List of all authors',
             resolve: () => authors
         },
     })
 })
 
+const RootMuatationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'Root Mutation',
+    fields: () => ({
+        addBook: {
+            type: BookType,
+            deprecation: 'Add a book',
+            args: {
+                name: { type:  GraphQLNonNull(GraphQLString) },
+                authorId: { type:  GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
+                books.push(book)
+                return book
+            }
+        },
+        addAuthor: {
+            type: AuthorType,
+            deprecation: 'Add a author',
+            args: {
+                name: { type:  GraphQLNonNull(GraphQLString) },
+            },
+            resolve: (parent, args) => {
+                const author = { id: author.length + 1, name: args.name }
+                authors.push(author)
+                return author
+            }
+        }
+    })
+})
 const schema = new GraphQLSchema({
-    query: RootQueryType
+    query: RootQueryType,
+    mutation: RootMuatationType
 })
 
 app.use('/graphql', expressGraphQL({
